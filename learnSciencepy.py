1b0d7a86937024974d03ba6b151c0a8ea749e17c
kelvinkvch <kelvinkv@126.com>
2020-01-13 15:23:03 +0800

    del myfirst.py

--- learnSciencepy.py
+++ learnSciencepy.py
@@ -1,13 +1,14 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-sns.set()
+
 index=[('california',2000),('california',2010),
         ('new york',2000),('new york',2010),
         ('texas',2000),('texas',2010)]
 populations=[33871648,37253956,
             18976457,19378102,
             20851820,25145561]
+index=pd.MultiIndex.from_tuples(index)
 pop=pd.Series(populations,index=index)
+pop=pop.reindex(index)
+
 print(pop)
