e0113988ce833754d9d4d1307f92a7be312d707e
kelvinkvch <kelvinkv@126.com>
2020-01-13 21:15:57 +0800

    panas multiIndex

--- learnSciencepy.py
+++ learnSciencepy.py
@@ -1,14 +1,13 @@
 import numpy as np
 import pandas as pd
-
-index=[('california',2000),('california',2010),
-        ('new york',2000),('new york',2010),
-        ('texas',2000),('texas',2010)]
-populations=[33871648,37253956,
-            18976457,19378102,
-            20851820,25145561]
-pop=pd.Series(populations,index=index)
+index = [('California', 2000), ('California', 2010),
+         ('New York', 2000), ('New York', 2010),
+         ('Texas', 2000), ('Texas', 2010)]
+populations = [33871648, 37253956, 18975457, 19378102, 20851820, 25145561]
 index=pd.MultiIndex.from_tuples(index)
-pop=pop.reindex(index)
-pop_df=pop.unstack()
-print(pop_df)
+pop=pd.Series(populations,index=index)
+under18=[9265089,9284094,4687374,4318033,5906301,6879014]
+popdf=pd.DataFrame({'total':pop,'under18':under18})
+popdf['f18']=popdf['under18']/popdf['total']
+print(popdf)
+# print(f_u18.unstack())
