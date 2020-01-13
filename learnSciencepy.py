7943e213630a577cb5d7966fa14976e9c540924d
kelvinkvch <kelvinkv@126.com>
2020-01-13 15:37:04 +0800

    modify learnsciencepy

--- learnSciencepy.py
+++ learnSciencepy.py
@@ -7,8 +7,7 @@ index=[('california',2000),('california',2010),
 populations=[33871648,37253956,
             18976457,19378102,
             20851820,25145561]
-index=pd.MultiIndex.from_tuples(index)
 pop=pd.Series(populations,index=index)
+index=pd.MultiIndex.from_tuples(index)
 pop=pop.reindex(index)
-
-print(pop[:,2010])
+print(pop)
