fdee8924d989c38afb2ece0abbeaffd13782b8f3
kelvinkvch <kelvinkv@126.com>
2020-01-14 08:25:52 +0800

    mkdirs

--- code/learnSciencepy.py
+++ code/learnSciencepy.py
@@ -7,8 +7,9 @@ index=[('california',2000),('california',2010),
 populations=[33871648,37253956,
             18976457,19378102,
             20851820,25145561]
-pop=pd.Series(populations,index=index)
 index=pd.MultiIndex.from_tuples(index)
-pop=pop.reindex(index)
-pop_df=pop.unstack()
-print(pop_df)
+pop=pd.Series(populations,index=index)
+pop.index.names=['state','year']
+# pop=pd.DataFrame(pop)
+pop=pop.reset_index(name='population')
+pop.to_csv('proutfile\out.csv',index=False)
