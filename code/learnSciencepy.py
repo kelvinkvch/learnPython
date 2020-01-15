4e8b88d43038e12d8314fa66409b366990ad7bb3
kelvinkvch <kelvinkv@126.com>
2020-01-15 23:23:04 +0800

    usepop

--- code/learnSciencepy.py
+++ code/learnSciencepy.py
@@ -1,15 +1,19 @@
 import numpy as np
 import pandas as pd
+f1 = r"D:\codepractice\python\PythonDataScienceHandbook\notebooks\data\state-population.csv"
+f2 = r"D:\codepractice\python\PythonDataScienceHandbook\notebooks\data\state-areas.csv"
+f3 = r"D:\codepractice\python\PythonDataScienceHandbook\notebooks\data\state-abbrevs.csv"
+pop = pd.read_csv(f1)
+areas = pd.read_csv(f2)
+abbrevs = pd.read_csv(f3)
+merged=pd.merge(pop,abbrevs,how='outer',left_on='state/region', right_on='abbreviation').drop('abbreviation', 1)
+merged.loc[merged['state/region']=='USA','state']='Unit states'
+merged.loc[merged['state/region']=='PR','state']='Puerto Rico'
+final=pd.merge(merged, areas,on='state',how='left')
+final=final.dropna()
+data2010=final.query("year==2010&ages=='total'")
+data2010=data2010.set_index('state')
+density=data2010['area (sq. mi)']/data2010['population']
 
-index=[('california',2000),('california',2010),
-        ('new york',2000),('new york',2010),
-        ('texas',2000),('texas',2010)]
-populations=[33871648,37253956,
-            18976457,19378102,
-            20851820,25145561]
-index=pd.MultiIndex.from_tuples(index)
-pop=pd.Series(populations,index=index)
-pop.index.names=['state','year']
-pop=pop.reset_index(name='population')
-pop.to_csv('proutfile\out.csv',index=False)
-
+density.sort_values(ascending=False,inplace=True)
+print(density.head())
