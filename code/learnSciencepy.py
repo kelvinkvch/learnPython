a8132bd9041b5c08e310d9299d8e824927b49e9d
kelvinkvch <kelvinkv@126.com>
2020-01-16 22:56:02 +0800

    pivot_table

--- code/learnSciencepy.py
+++ code/learnSciencepy.py
@@ -1,19 +1,6 @@
-import numpy as np
 import pandas as pd
-f1 = r"D:\codepractice\python\PythonDataScienceHandbook\notebooks\data\state-population.csv"
-f2 = r"D:\codepractice\python\PythonDataScienceHandbook\notebooks\data\state-areas.csv"
-f3 = r"D:\codepractice\python\PythonDataScienceHandbook\notebooks\data\state-abbrevs.csv"
-pop = pd.read_csv(f1)
-areas = pd.read_csv(f2)
-abbrevs = pd.read_csv(f3)
-merged=pd.merge(pop,abbrevs,how='outer',left_on='state/region', right_on='abbreviation').drop('abbreviation', 1)
-merged.loc[merged['state/region']=='USA','state']='Unit states'
-merged.loc[merged['state/region']=='PR','state']='Puerto Rico'
-final=pd.merge(merged, areas,on='state',how='left')
-final=final.dropna()
-data2010=final.query("year==2010&ages=='total'")
-data2010=data2010.set_index('state')
-density=data2010['area (sq. mi)']/data2010['population']
-
-density.sort_values(ascending=False,inplace=True)
-print(density.head())
+import numpy as np
+import seaborn as sns
+titanic=sns.load_dataset('titanic')
+pvt=titanic.pivot_table('survived', index='class',columns='sex')*100
+print(pvt)
