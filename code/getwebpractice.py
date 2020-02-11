f619acc460cc4a50792cc987aa49de843a4ab1bf
kelvinkvch <kelvinkv@126.com>
2020-02-11 21:08:04 +0800

    add python conbine files

--- code/getwebpractice.py
+++ code/getwebpractice.py
@@ -1,9 +1,7 @@
-import requests
 from lxml import etree
-from bs4 import BeautifulSoup as bsp
-url=r'https://news.ifeng.com/c/special/7tPlDSzDgVk'
-hds={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36'}
-web=requests.get(url,headers=hds)
+from lxml import  html
+url=r'http://example.python-scraping.com/view/Botswana-30'
+import requests
+web=requests.get(url)
 tree=etree.HTML(web.text)
-rows=tree.xpath('//div[@class="tbody-3sEej9JG"]/div')
-print(rows)
+print(etree.text())a
